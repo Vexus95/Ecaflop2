9ea22a8c626503b618fb4dbae349c6a100994795
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-12-11 20:14:44 +0100

    This is now an REST (like) API

--- python/hr/urls.py
+++ python/hr/urls.py
@@ -7,32 +7,7 @@ app_name = "hr"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("api/employees", views.employees, name="employees"),
-    path("api/employee/<int:pk>", views.employee, name="employee"),
-    path("api/employee/new", views.new_employee, name="new_employee"),
-    path(
-        "employees",
-        views.EmployeeListView.as_view(),
-        name="employee_list",
-    ),
-    path(
-        "employee/create",
-        views.employee_create,
-        name="employee_create",
-    ),
-    path(
-        "employee/<int:pk>/update",
-        views.employee_update,
-        name="employee_update",
-    ),
-    path(
-        "employee/<int:pk>/delete",
-        views.EmployeeDeleteView.as_view(),
-        name="employee_delete",
-    ),
-    path(
-        "clean_db",
-        views.clean_db,
-        name="clean_db",
-    ),
+    path("api/v1/employees", views.list_employees, name="employees"),
+    path("api/v1/employee/<int:pk>", views.update_employee, name="employee"),
+    path("api/v1/employee", views.new_employee, name="new_employee"),
 ]
--- python/hr/views.py
+++ python/hr/views.py
@@ -15,7 +15,7 @@ from .models import Employee
 
 
 @csrf_exempt
-def employees(request):
+def list_employees(request):
     rows = Employee.objects.all()
     as_json = [x.to_json() for x in rows]
     return JsonResponse(
@@ -38,7 +38,8 @@ def employee(request, pk):
         payload = json.loads(body)
         employee.update(payload)
         employee.save()
-        return JsonResponse({"status": "updated"})
+        return JsonResponse({"employee": employee.to_json()}, status_code=202)
+
 
 @csrf_exempt
 def new_employee(request):
@@ -47,129 +48,10 @@ def new_employee(request):
     employee = Employee()
     employee.update(payload)
     employee.save()
-    return JsonResponse({"status": "created"})
+    return JsonResponse({"employee": employee.to_json()}, status_code=201)
 
 
-def index(request):
-    return TemplateResponse(request, "hr/index.haml")
-
-
+@csrf_exempt
 def clean_db(request):
-    if request.method == "GET":
-        return TemplateResponse(request, "hr/db_confirm_delete.haml")
-    else:
-        Employee.objects.all().delete()
-        return redirect("hr:index")
-
-
-# Using Class-Based-Views here because I don't inted to customize those
-class EmployeeListView(ListView):
-    model = Employee
-    template_name = "hr/employee_list.haml"
-
-
-class EmployeeDeleteView(DeleteView):
-    model = Employee
-    template_name = "hr/employee_confirm_delete.haml"
-    success_url = reverse_lazy("hr:employee_list")
-
-
-def employee_create(request):
-    if request.method == "GET":
-        return employee_create_form(request, context={})
-    else:
-        return employee_create_action(request)
-
-
-def employee_create_form(request, *, context):
-    context.update(
-        {
-            "action": "create",
-            "title": "Adding new employee",
-            "form_url": reverse_lazy("hr:employee_create"),
-        }
-    )
-    return TemplateResponse(request, "hr/employee_create.haml", context)
-
-
-def employee_create_action(request):
-    data, errors = process_form(
-        request,
-        required_fields=[
-            "name",
-            "address_line1",
-            "city",
-            "zip_code",
-            "email",
-        ],
-        optional_fields=["address_line2"],
-    )
-    if errors:
-        context = {"errors": errors, "form": data}
-        return employee_create_form(request, context=context)
-    else:
-        model = Employee(**data)
-        model.save()
-        return redirect("hr:employee_list")
-
-
-def employee_update(request, pk):
-    if request.method == "GET":
-        return employee_update_form(request, pk=pk, context={})
-    else:
-        return employee_update_action(request, pk)
-
-
-def employee_update_form(request, *, pk, context):
-    employee = get_object_or_404(Employee, pk=pk)
-    context.update(
-        {
-            "action": "Update",
-            "title": "Update new employee",
-            "form": employee,
-            "form_url": reverse_lazy("hr:employee_update", kwargs={"pk": pk}),
-            "delete": True,
-        }
-    )
-    return TemplateResponse(request, "hr/employee_update.haml", context)
-
-
-def employee_update_action(request, pk):
-    data, errors = process_form(
-        request,
-        required_fields=[
-            "name",
-            "address_line1",
-            "city",
-            "zip_code",
-            "email",
-        ],
-        optional_fields=["address_line2"],
-    )
-    if errors:
-        context = {"errors": errors}
-        return employee_update_form(request, pk=pk, context=context)
-    else:
-        model = Employee(pk=pk, **data)
-        model.save()
-        return redirect("hr:employee_list")
-
-
-def process_form(request, *, required_fields=None, optional_fields=None):
-    form = request.POST
-    required_fields = required_fields or []
-    optional_fields = optional_fields or []
-    errors = []
-    data = {}
-    for key in required_fields:
-        value = form.get(key, "")
-        if value:
-            data[key] = value
-        else:
-            errors.append(f"Field '{key}' is required")
-    for key in optional_fields:
-        if form.get(key):
-            value = form.get(key, "")
-            if value:
-                data[key] = value
-    return data, errors
+    Employee.objects.all().delete()
+    return JsonResponse({"message": "database reset"}, status_code=202)
