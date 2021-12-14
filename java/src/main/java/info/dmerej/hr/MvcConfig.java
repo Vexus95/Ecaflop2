de6225e96a0be09165d33345769ad4a624b8c013
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-12-14 14:39:20 +0100

    java: fix cors config

--- java/src/main/java/info/dmerej/hr/MvcConfig.java
+++ java/src/main/java/info/dmerej/hr/MvcConfig.java
@@ -10,6 +10,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**").allowedOrigins("http://127.0.0.1:8080");
+    registry
+      .addMapping("/**")
+      .allowedOrigins("http://127.0.0.1:8080")
+      .allowedMethods("*");
   }
 }
