8e2f29d1f04aa3e7eaa007f0e865bed1f6cdf282
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-12-23 11:47:14 +0100

    Fix javascript unit tests

--- javascript/tests/repository.test.js
+++ javascript/tests/repository.test.js
@@ -1,6 +1,15 @@
 const Repository = require('../src/repository')
 const assert = require('chai').assert
 
+const JOHN = {
+  name: 'John',
+  email: 'john@corp.tld',
+  address_line1: 'line 1',
+  address_line2: 'line 2',
+  city: 'paris',
+  zip_code: '75001'
+}
+
 describe('repository', () => {
   it('can be initialized', async () => {
     const repository = new Repository(':memory:')
@@ -10,16 +19,25 @@ describe('repository', () => {
   it('can insert employees', async () => {
     const repository = new Repository(':memory:')
     await repository.init()
-    await repository.insertEmployee({ name: 'John', email: 'john@corp.tld' })
+    const id = await repository.insertEmployee(JOHN)
+    assert.isAtLeast(1, id)
   })
 
   it('can retrieve employees', async () => {
     const repository = new Repository(':memory:')
     await repository.init()
-    const id = await repository.insertEmployee({ name: 'John', email: 'john@corp.tld' })
+    const id = await repository.insertEmployee(
+      {
+        name: 'John',
+        email: 'john@corp.tld',
+        address_line1: 'line 1',
+        address_line2: 'line 2',
+        city: 'paris',
+        zip_code: '75001'
+      })
 
     const actual = await repository.getEmployeeById(id)
-
-    assert.deepEqual(actual, { name: 'John', email: 'john@corp.tld', id })
+    const expected = { id, ...JOHN }
+    assert.deepEqual(actual, expected)
   })
 })
