bf6cce839ee612e9118340619038fdae2844fbe3
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-12-29 13:49:46 +0100

    Employee fields cannot be null and have no default

--- python/hr/models.py
+++ python/hr/models.py
@@ -4,10 +4,10 @@ from django.db import models
 class Employee(models.Model):
     name = models.CharField(null=False, max_length=100)
     address_line1 = models.CharField(null=False, max_length=100)
-    address_line2 = models.CharField(null=True, max_length=100, default="")
+    address_line2 = models.CharField(null=False, max_length=100)
     city = models.CharField(null=False, max_length=100)
     zip_code = models.CharField(null=False, max_length=100)
-    email = models.EmailField(null=True)
+    email = models.EmailField(null=False)
 
     def __str__(self):
         return f"{self.name} - {self.email}"
