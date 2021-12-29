fe8bfaeccb22d90e13c784c2628aaab275cceaeb
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-12-29 13:49:32 +0100

    Kill Employee.to_json()

--- python/hr/models.py
+++ python/hr/models.py
@@ -10,18 +10,4 @@ class Employee(models.Model):
     email = models.EmailField(null=True)
 
     def __str__(self):
-        import json
-
-        return json.dumps(self.to_json())
-
-    def to_json(self):
-        res = {
-            "name": self.name,
-            "email": self.email,
-            "address_line1": self.address_line1,
-            "address_line2": self.address_line2,
-            "city": self.city,
-            "zip_code": self.zip_code,
-        }
-        res["id"] = self.pk
-        return res
+        return f"{self.name} - {self.email}"
