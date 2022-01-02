36181b8fad229a2fe6cd5de508f1daaa2ee4f376
Dimitri Merejkowsky <dimitri@dmerej.info>
2022-01-02 16:38:37 +0100

    Add more tests

--- python/tests/integration_tests.py
+++ python/tests/integration_tests.py
@@ -40,7 +40,17 @@ def find_employee_row(page, employee_name):
         if employee_name in row.inner_html():
             return row
 
-    pytest.fail(f"{employee_name} not found in the list")
+    pytest.fail(f"employee {employee_name} not found in the list")
+
+
+def find_team_row(page, team_name):
+    tables_rows = page.locator("tr")
+    for i in range(0, tables_rows.count()):
+        row = tables_rows.nth(i)
+        if team_name in row.inner_html():
+            return row
+
+    pytest.fail(f"team {team_name} not found in the list")
 
 
 def test_add_employee(saved_employee, page):
@@ -128,6 +138,60 @@ def test_edit_employee_job_title(saved_employee, page):
     assert input_element.input_value() == new_value
 
 
+def create_team(page):
+    page.goto("/add_team")
+    name = page.locator('input[name="name"]')
+    fake = Faker()
+    team_name = fake.pystr()
+    name.fill(team_name)
+    page.click("text='Add'")
+
+    return team_name
+
+
+def test_create_team(page):
+    team_name = create_team(page)
+    assert page.is_visible(f"td:has-text('{team_name}')")
+
+
+def test_add_member_to_team(page):
+    team_name = create_team(page)
+
+    employee = new_fake_employee()
+    employee = save_employee(page, employee)
+    row = find_employee_row(page, employee.name)
+    edit_button = row.locator("text=Edit")
+    edit_button.click()
+
+    page.click("text='Add to team'")
+
+    select = page.locator("select[name='team_id']")
+    select.select_option(label=team_name)
+    page.click("text='Add'")
+
+    row = find_team_row(page, team_name)
+    link = row.locator("text='View members'")
+    url = link.get_attribute("href")
+    page.goto(url)
+    assert page.is_visible(f"ul:has-text('{employee.name}')")
+
+
+def test_promote_manager(page):
+    employee = new_fake_employee()
+    employee = save_employee(page, employee)
+    row = find_employee_row(page, employee.name)
+    edit_button = row.locator("text=Edit")
+    edit_button.click()
+
+    page.click("text='Promote as manager'")
+    page.click("text='Proceed'")
+
+    row = find_employee_row(page, employee.name)
+    cell = row.locator("td > strong")
+    assert cell.is_visible()
+    assert cell.inner_text() == "yes"
+
+
 def test_delete_single_employee(page):
     alice = new_fake_employee()
     bob = new_fake_employee()
@@ -144,3 +208,18 @@ def test_delete_single_employee(page):
 
     assert not page.is_visible(f"text={alice.name}")
     assert page.is_visible(f"text={bob.name}")
+
+
+def test_delete_team(page):
+    team_one = create_team(page)
+    team_two = create_team(page)
+
+    row = find_team_row(page, team_two)
+    link = row.locator("text='Delete'")
+    url = link.get_attribute("href")
+    page.goto(url)
+
+    page.click("text='Proceed'")
+
+    assert page.is_visible(f"td:has-text('{team_one}')")
+    assert not page.is_visible(f"td:has-text('{team_two}')")
