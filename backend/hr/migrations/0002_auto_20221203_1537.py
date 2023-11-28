7d5fd546ee26823dc66d1b6f178cc8d7bf177f0c
Dimitri Merejkowsky <dimitri@dmerej.info>
2023-11-28 17:31:52 +0100

    Paint it black

--- backend/hr/migrations/0002_auto_20221203_1537.py
+++ backend/hr/migrations/0002_auto_20221203_1537.py
@@ -4,24 +4,23 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('hr', '0001_initial'),
+        ("hr", "0001_initial"),
     ]
 
     operations = [
         migrations.RenameField(
-            model_name='address',
-            old_name='line1',
-            new_name='address_line1',
+            model_name="address",
+            old_name="line1",
+            new_name="address_line1",
         ),
         migrations.RemoveField(
-            model_name='address',
-            name='line2',
+            model_name="address",
+            name="line2",
         ),
         migrations.AddField(
-            model_name='address',
-            name='address_line2',
+            model_name="address",
+            name="address_line2",
             field=models.CharField(blank=True, max_length=100, null=True),
         ),
     ]
