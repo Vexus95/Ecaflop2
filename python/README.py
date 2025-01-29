7d7eb448433a0bc5ccf72d9136ea628157568ac2
Dimitri Merejkowsky <dimitri@dmerej.info>
2025-01-29 20:31:52 +0100

    Update README.py

--- python/README.py
+++ python/README.py
@@ -2,22 +2,16 @@
 
 ## Installing dependencies
 
+(Note: this is _not_ the same virtualenv as the backend!)
+
 ```
-python -b venv .venv
+python -b venv .venv --prompt hr-db-tests
 source .venv/bin/activate
 pip install -r requirements.txt
 ```
 
-
-## Running end-to-end tests
-
-```
-playwright install chromium
-pytest --base-url http://127.0.0.1:8000 --browser=chromium --headed end_to_end_tests --slowmo=1000
-```
-
-## Running integration tests
+## Running the tests
 
 ```
-pytest integration_tests
+pytest 
 ```
